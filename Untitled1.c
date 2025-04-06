93f024b21394da9ffa374b96dfedff0923fe86f9
YakibchukOstap567 <ostap.yakibchuk02@gmail.com>
2025-04-07 00:24:20 +0200

    Urobil som hlavnu(vyber znakov) a pomocnu(vymena znakov) funkciu

--- Untitled1.c
+++ Untitled1.c
@@ -1,14 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define usp 1
 #define zlyh 0
+
+void vymena(char *a,char *b){
+	char c=*a;
+	*a=*b;
+	*b=c;
+}
 
 char nahodna_permutacia_abecedy(char *str, char l){
 	if(l<1 || l>26){
 		return zlyh;
 	}
 	
+	char abc[27];
 	
+	for(int i=0; i<26; i++){
+		abc[i]='a'+i;
+	}
+	
+	for(int p=0; p<l; p++){
+		int q=rand()%(p+1);
+		vymena(&abc[p],&abc[q]);
+	}
+	
+	strncpy(str,abc,l);
 	
 	return usp;
 }
@@ -17,7 +37,9 @@ int main() {
 	char vys[27];
 	char d=7;
 	
-	if(nahodna_permutacia_abecedy(vys, d) == usp){
+	srand(time(NULL));
+	
+	if(nahodna_permutacia_abecedy(vys, d)==usp){
 		printf("Nachodna permutacia prvych %d znakov: %s \n", d,vys);
 	}
 	else{
