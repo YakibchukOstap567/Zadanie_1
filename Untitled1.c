e6322913ccd1d0b22515a62b899bce5df1461aaf
YakibchukOstap567 <ostap.yakibchuk02@gmail.com>
2025-04-06 22:33:19 +0200

    Urobil som co vypyse na konci programu ak bude zlychanie alebo uspech

--- Untitled1.c
+++ Untitled1.c
@@ -1,1 +1,28 @@
 #include<stdio.h>
+
+#define usp 1
+#define zlyh 0
+
+char nahodna_permutacia_abecedy(char *str, char l){
+	if(l<1 || l>26){
+		return zlyh;
+	}
+	
+	
+	
+	return usp;
+}
+
+int main() {
+	char vys[27];
+	char d=7;
+	
+	if(nahodna_permutacia_abecedy(vys, d) == usp){
+		printf("Nachodna permutacia prvych %d znakov: %s \n", d,vys);
+	}
+	else{
+		printf("Chyba: nespravna dlzka", d,vys);
+	}
+	
+	return 0;
+}
